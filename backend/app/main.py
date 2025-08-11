661a947d476bb02c9c277380e001852b242c0e46
Aswajith S <139551991+Aswajith7077@users.noreply.github.com>
2025-08-11 08:00:22 +0530

    change main remove print

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,9 +7,7 @@ from config.config import front_end_dev_url, front_end_prod_url
 
 app = FastAPI(root_path="/api")
 
-origins = ["*"]
-
-print(front_end_dev_url, front_end_prod_url)
+origins = [front_end_dev_url, front_end_prod_url]
 
 app.add_middleware(
     CORSMiddleware,
