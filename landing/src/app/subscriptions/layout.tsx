72110e78ba31d6c1fc42f94670145c3ad69e6ac4
Aswajith S <139551991+Aswajith7077@users.noreply.github.com>
2025-09-07 22:14:35 +0530

    feat: add landing page

---
+++ landing/src/app/subscriptions/layout.tsx
@@ -0,0 +1,12 @@
+import ElectricBorderCard from "@/components/animations/pricingCard";
+
+
+const Page = () => {
+    return <div className="w-full h-screen">
+        <div>
+            
+        </div>
+    </div>;
+}
+
+export default Page;
--- landing/src/components/Footer.tsx
+++ landing/src/components/Footer.tsx
@@ -1,37 +1,8 @@
-import { Link } from 'react-router-dom';
-import { Button } from '../ui/button';
-import { Input } from '../ui/input';
-import { AppName, LogoIcon } from '@/constants/app.details';
-
-const contents = [
-  {
-    title: 'Quick Links',
-    links: [
-      { name: 'Home', url: '/' },
-      { name: 'Pricing', url: '/pricing' },
-      { name: 'About Us', url: '/about' },
-      { name: 'faq', url: '/faq' },
-      { name: 'Contact', url: '/contact' },
-    ],
-  },
-  {
-    title: 'Resources',
-    links: [
-      { name: 'Deals & Promotions', url: '/deals' },
-      { name: 'Blog', url: '/blog' },
-      { name: 'Help Center', url: '/help' },
-      { name: 'Support', url: '/support' },
-    ],
-  },
-  {
-    title: 'Legal',
-    links: [
-      { name: 'Privacy Policy', url: '/privacy' },
-      { name: 'Terms of Use', url: '/terms-of-use' },
-      { name: 'Terms of Service', url: '/terms' },
-    ],
-  },
-];
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { appname as AppName, LogoIcon } from "@/constants/app";
+import { contents } from "@/constants/footer";
 
 const Footer = () => {
   return (
@@ -61,10 +32,10 @@ const Footer = () => {
                   {content.links.map((link, linkIndex) => (
                     <Button
                       key={linkIndex}
-                      variant={'link'}
+                      variant={"link"}
                       className="text-sm h-7 text-left mt-1"
                     >
-                      <Link to={link.url} className="text-gray-400 w-full">
+                      <Link href={link.url} className="text-gray-400 w-full">
                         {link.name}
                       </Link>
                     </Button>
@@ -74,7 +45,7 @@ const Footer = () => {
             );
           })}
         </div>
-        <div className="flex flex-col md:w-1/5 gap-6 py-4 lg:w-[25%] mr-10 ">
+        {/* <div className="flex flex-col md:w-1/5 gap-6 py-4 lg:w-[25%] mr-10 ">
           <h2 className="text-lg pr-4 w-full text-left font-semibold">
             {'Get Updates'}
           </h2>
@@ -92,12 +63,32 @@ const Footer = () => {
               Subscribe
             </Button>
           </div>
-        </div>
+        </div> */}
       </div>
+			<div className="flex flex-col items-center justify-center gap-6 py-4 px-4 ">
+            <h2 className="text-lg pr-4 w-full text-center font-semibold">
+              {"Get Updates"}
+            </h2>
+            <p className="font-open-sans text-[14px] text-gray-400">
+              By this you can stay up to date with the latest tech and updates
+              with Zevrin
+            </p>
+            <div className="flex flex-row w-3/4 justify-center items-center gap-5">
+              <Input
+                type="email"
+                placeholder="Enter your email"
+                className="mt-2 px-4 py-2 bg-gray-900 text-gray-400 rounded-full border-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              <Button
+                className="mt-3 w-1/4 rounded-full px-5"
+                variant={"ghost"}
+              >
+                Subscribe
+              </Button>
+            </div>
+          </div>
       <div className="flex flex-row w-[95%] items-center justify-center pt-7 my-3">
-        <p className="text-sm" suppressHydrationWarning>
-          {`© ${new Date().getFullYear()} Zevrin. All rights reserved.`}
-        </p>
+        <p className="text-sm">{"© 2023 Zevrin. All rights reserved."}</p>
       </div>
     </footer>
   );
